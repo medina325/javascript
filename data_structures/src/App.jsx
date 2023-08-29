222e96f58da25310c4710218680ca8b57db1c4eb
medina325 <medina_cdz@hotmail.com>
2023-08-28 21:42:02 -0400

    Implementando componentes Tree e FlatTree para exemplo de uso de árvores

--- data_structures/src/App.jsx
+++ data_structures/src/App.jsx
@@ -1,9 +1,11 @@
+import FlatTree from "./FlatTree.jsx";
 import Tree from "./Tree.jsx";
 
 export default function App() {
   return (
     <div className="container">
       <Tree />
+      <FlatTree />
     </div>
   );
-};
+}
