ad76c78c9190879d2ead8b8abd8fec92f7020db8
medina325 <medina_cdz@hotmail.com>
2023-08-29 10:54:35 -0400

    Arrumando style de arquivos jsx

--- data_structures/src/index.jsx
+++ data_structures/src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
