34d9165019512124366061e3ee8890612ce949b8
medina325 <medina_cdz@hotmail.com>
2023-09-02 16:54:40 -0400

    Criando wrapper "example" divs para deixar tudo mais bonitinho

--- data_structures/src/App.jsx
+++ data_structures/src/App.jsx
@@ -4,8 +4,14 @@ import Tree from "./Tree.jsx";
 export default function App() {
   return (
     <div className="container">
-      <Tree />
-      <FlatTree />
+      <div className="example">
+        <h3>Tree</h3>
+        <Tree />
+      </div>
+      <div className="example">
+        <h3>Flat Tree</h3>
+        <FlatTree />
+      </div>
     </div>
   );
 }
